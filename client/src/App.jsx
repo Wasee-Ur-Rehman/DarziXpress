6ccad20c670bd005948cae0dca2550208ee43980
Hasan Yahya <118683092+HasanYahya101@users.noreply.github.com>
2025-04-20 21:06:57 +0500

    Fix a few imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { Button } from './components/ui/button';
 import { ThemeProvider } from './components/theme-provider';
 
 function App() {
@@ -10,13 +9,6 @@ function App() {
       <div className="min-h-screen min-w-screen bg-white dark:bg-black items-center justify-center">
         <h1 className="text-center pt-[34vh]">Vite + React</h1>
         <div className="p-8 flex flex-col items-center">
-          <Button
-            variant="outline"
-            className="mb-4"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is {count}
-          </Button>
           <p className="text-center">
             Edit <code>src/App.jsx</code> and save to test HMR
           </p>
