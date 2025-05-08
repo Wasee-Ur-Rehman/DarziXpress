4cc8c237b870f3c4e5a04c9dc698990cbcb18918
Wasee-Ur-Rehman <amassed971@gmail.com>
2025-05-09 00:39:48 +0500

    Added Landing Page"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,8 @@
-import { useState } from 'react';
-import { ThemeProvider } from './components/theme-provider';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <div className="min-h-screen min-w-screen bg-white dark:bg-black items-center justify-center">
-        <h1 className="text-center pt-[34vh]">Vite + React</h1>
-        <div className="p-8 flex flex-col items-center">
-          <p className="text-center">
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="text-gray-600 text-center">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
-    </ThemeProvider>
+    <div>Hello World</div>
   )
 }
 
