91c7150d16739c17ba721155e4929c2d1eacab10
Fatima Binte Tariq <fatimabintetariq410@gmail.com>
2025-05-11 23:58:25 +0500

    Resolve merge conflicts

--- client/src/components/ui/label.jsx
+++
@@ -1,8 +0,0 @@
-// components/ui/label.js
-import React from 'react';
-
-export const Label = ({ htmlFor, children }) => (
-    <label htmlFor={htmlFor} className="block text-sm font-medium text-slate-700">
-        {children}
-    </label>
-);
